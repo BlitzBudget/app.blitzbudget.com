52cedd320c3a58d299978c130598d47edcac2cc7
D4901479 <nagarjun_nagesh@carrefour.com>
2019-12-16 17:34:55 +0100

    Fix minor bugs

--- resources/static/js/cognito/cognito-helper.js
+++ resources/static/js/cognito/cognito-helper.js
@@ -85,6 +85,16 @@ uh = {
 	            cognitoUser.verifyAttribute('email', verificationCode, this);
 	        }
 		 });
+	},
+
+	globalSignout() {
+		var params = {
+		  AccessToken: 'STRING_VALUE' /* required */
+		};
+		cognitoUser.globalSignOut(params, function(err, data) {
+		  if (err) console.log(err, err.stack); // an error occurred
+		  else     console.log(data);           // successful response
+		});
 	}
 }
 
