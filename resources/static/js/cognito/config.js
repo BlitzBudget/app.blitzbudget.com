9872e5708d8a0519e422e7e6e205a140598dcd86
Arjun <nagarjun_nagesh@outlook.com>
2019-12-30 19:23:09 +0100

    profile js for reset account

--- resources/static/js/cognito/config.js
+++ resources/static/js/cognito/config.js
@@ -6,5 +6,8 @@ window._config = {
     },
     api: {
         invokeUrl: 'https://api.blitzbudget.com' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    },
+    home: {
+    	invokeUrl: 'https://www.blitzbudget.com'
     }
 };
