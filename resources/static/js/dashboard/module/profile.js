1f3abba481bdc4610e4d768f504c3d7ba384bb43
Arjun <nagarjun_nagesh@outlook.com>
2020-01-02 22:09:18 +0100

    edit tool tip for change password

--- resources/static/js/dashboard/module/profile.js
+++ resources/static/js/dashboard/module/profile.js
@@ -212,11 +212,12 @@
 		svgElement.setAttribute('width','20');
 		svgElement.setAttribute('height','20');
     	svgElement.setAttribute('viewBox','0 0 24 24');
-    	svgElement.setAttribute('class','align-middle fill-black info-pass');
-    	svgElement.setAttribute("title", "Minimum 8 characters");
-		svgElement.setAttribute("data-toggle", "tooltip");
-		svgElement.setAttribute("data-placement", "right");
-    	
+    	svgElement.setAttribute('class','align-middle fill-black ml-1');
+    	svgElement.setAttribute('id', 'input-pass-cp');
+    	svgElement.setAttribute('data-original-title', 'Your password must: <br> <ul><li>Be at least 8 characters</li><li>Have at least one number</li><li>Have at least one symbol</li><li>Have at least one upper case letter</li><li>Have at least one lower case letter</li></ul>');
+    	svgElement.setAttribute('data-content', '');
+    	svgElement.setAttribute('data-placement', 'right');
+
     	let pathElement1 = document.createElementNS("http://www.w3.org/2000/svg", 'path');
     	pathElement1.setAttribute('d','M 12 2 C 6.4889971 2 2 6.4889971 2 12 C 2 17.511003 6.4889971 22 12 22 C 17.511003 22 22 17.511003 22 12 C 22 6.4889971 17.511003 2 12 2 z M 12 4 C 16.430123 4 20 7.5698774 20 12 C 20 16.430123 16.430123 20 12 20 C 7.5698774 20 4 16.430123 4 12 C 4 7.5698774 7.5698774 4 12 4 z M 11 7 L 11 9 L 13 9 L 13 7 L 11 7 z M 11 11 L 11 17 L 13 17 L 13 11 L 11 11 z'); 
     	svgElement.appendChild(pathElement1);
@@ -300,6 +301,13 @@
                 changePassword(oldPassword, newPassword, cognitoUser);
             }
 
+        });
+
+        //$("#input-pass-cp").popover({ animation:true, delay: 200, trigger:"focus", placement: "right" });
+        //$("#input-pass-cp").popover("show");
+        $("#input-pass-cp").tooltip({
+        	html: true,
+			delay: { "show": 300, "hide": 100 }
         });
 
         // Disable Change Password button 
