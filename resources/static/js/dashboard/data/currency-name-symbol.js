d0730d781475bac5218720dbe787aa8ff5faba54
D4901479 <nagarjun_nagesh@carrefour.com>
2020-01-08 17:01:56 +0100

    currency information to js and settings

---
+++ resources/static/js/dashboard/data/currency-name-symbol.js
@@ -0,0 +1,697 @@
+/* 
+* currency-name-to-symbol Object which maps the relationship between currency name and currency symbol 
+*/
+// JSON equivalent
+//{"currency-name-to-symbol":[{"currency":"Afghan Afghani","symbol":"؋"},{"currency":"Euro","symbol":"€"},{"currency":"Albanian Lek","symbol":"L"},{"currency":"Algerian Dinar","symbol":"دج"},{"currency":"US Dollar","symbol":"$"},{"currency":"Angolan Kwanza","symbol":"Kz"},{"currency":"East Caribbean Dollar","symbol":"EC$"},{"currency":"Argentine Peso","symbol":"$"},{"currency":"Armenian Dram","symbol":"֏"},{"currency":"Aruban Florin","symbol":"ƒ"},{"currency":"Australian Dollar","symbol":"AU$"},{"currency":"Azerbaijan Manat","symbol":"₼"},{"currency":"Bahamian Dollar","symbol":"B$"},{"currency":"Bahraini Dinar","symbol":".د.ب"},{"currency":"Bangladeshi Taka","symbol":"Tk"},{"currency":"Barbados Dollar","symbol":"Bds$"},{"currency":"Belarusian Ruble","symbol":"Br"},{"currency":"Belize Dollar","symbol":"BZ$"},{"currency":"CFA Franc","symbol":"CFA"},{"currency":"Bermudian Dollar","symbol":"BD$"},{"currency":"Bhutanese Ngultrum","symbol":"Nu."},{"currency":"Boliviano","symbol":"Bs"},{"currency":"Convertible Mark","symbol":"KM"},{"currency":"Pula","symbol":"P"},{"currency":"Norwegian Krone","symbol":"kr"},{"currency":"Brazilian Real","symbol":"R$"},{"currency":"Brunei Dollar","symbol":"B$"},{"currency":"Bulgarian Lev","symbol":"Лв."},{"currency":"CFA Franc BCEAO","symbol":"CFA"},{"currency":"Burundi Franc","symbol":"FBu"},{"currency":"Cape Verde Escudo","symbol":"$"},{"currency":"Cambodian Riel","symbol":"៛"},{"currency":"CFA Franc BEAC","symbol":"FCFA"},{"currency":"Canadian Dollar","symbol":"C$"},{"currency":"Cayman Islands Dollar","symbol":"CI$"},{"currency":"Chilean Peso","symbol":"$"},{"currency":"Renminbi (Yuan)","symbol":"¥"},{"currency":"Colombian Peso","symbol":"$"},{"currency":"Comoro Franc","symbol":"CF"},{"currency":"CFA Franc BEAC","symbol":"FC"},{"currency":"Franc Congolais","symbol":"FC"},{"currency":"New Zealand Dollar","symbol":"NZ$"},{"currency":"Costa Rican Colón","symbol":"₡"},{"currency":"Croatian Kuna","symbol":"kn"},{"currency":"Cuban Peso","symbol":"$"},{"currency":"Antillean Guilder","symbol":"NAƒ"},{"currency":"Czech Koruna","symbol":"Kč"},{"currency":"Danish Kroner","symbol":"Kr."},{"currency":"Djibouti Franc","symbol":"Fdj"},{"currency":"Dominican Peso","symbol":"RD$"},{"currency":"Egyptian Pound","symbol":"E£"},{"currency":"Nakfa","symbol":"Nfk"},{"currency":"Swazi lilangeni","symbol":"L"},{"currency":"Ethiopian Birr","symbol":"Br"},{"currency":"Falkland Islands pound","symbol":"£"},{"currency":"Faroese króna (notes only), the Danish krone is also in use","symbol":"Kr."},{"currency":"Fiji Dollar","symbol":"FJ$"},{"currency":"CFP Franc","symbol":"₣"},{"currency":"Dalasi","symbol":"D"},{"currency":"Lari","symbol":"ლ"},{"currency":"Cedi","symbol":"GH₵"},{"currency":"Gibraltar Pound","symbol":"£"},{"currency":"Danish Krone","symbol":"Kr."},{"currency":"Quetzal","symbol":"GTQ"},{"currency":"Guernsey pound","symbol":"£"},{"currency":"Guinea Franc","symbol":"FG"},{"currency":"Guyana Dollar","symbol":"GY$"},{"currency":"Haitian Gourde\r\n","symbol":"G"},{"currency":"Lempira","symbol":"L"},{"currency":"Hong Kong Dollar","symbol":"HK$"},{"currency":"Forint","symbol":"Ft"},{"currency":"Iceland Krona","symbol":"Íkr"},{"currency":"Indian Rupee","symbol":"₹"},{"currency":"Rupiah","symbol":"Rp"},{"currency":"Iranian Rial","symbol":"﷼"},{"currency":"Iraqi Dinar","symbol":"ع.د"},{"currency":"Manx pound","symbol":"£"},{"currency":"New Israeli Sheqel","symbol":"₪"},{"currency":"Jamaican Dollar","symbol":"J$"},{"currency":"Yen","symbol":"円"},{"currency":"Jersey pound","symbol":"£"},{"currency":"Jordanian Dinar","symbol":"د.أ"},{"currency":"Tenge","symbol":"₸"},{"currency":"Kenyan Shilling","symbol":"K"},{"currency":"North Korean Won","symbol":"₩"},{"currency":"Won","symbol":"₩"},{"currency":"Kuwaiti Dinar","symbol":"د.ك"},{"currency":"Som","symbol":"Лв"},{"currency":"Kip","symbol":"₭"},{"currency":"Lebanese Pound","symbol":"ل.ل."},{"currency":"Loti (plural Maloti). It is equivalent to the South African Rand.","symbol":"L"},{"currency":"Liberian Dollar","symbol":"L$"},{"currency":"Lybian Dinar","symbol":"ل.د"},{"currency":"Swiss Franc","symbol":"CHF"},{"currency":"Macanese Pataca","symbol":"MOP$"},{"currency":"Malagasy Franc","symbol":"Ar"},{"currency":"Kwacha","symbol":"MK"},{"currency":"Malaysian Ringgit","symbol":"RM"},{"currency":"Rufiyaa","symbol":".ރ"},{"currency":"Maltese Lira","symbol":"€"},{"currency":"Ouguiya","symbol":"UM"},{"currency":"Mauritius Rupee","symbol":"₨"},{"currency":"Mexican Peso","symbol":"Mex$"},{"currency":"Moldovan Leu","symbol":"L"},{"currency":"Mongolia Tughrik","symbol":"₮"},{"currency":"Moroccan Dirham","symbol":". د. م"},{"currency":"Mozambican Metical","symbol":"MT"},{"currency":"Burmese Kyat","symbol":"K"},{"currency":"Namibia Dollar\r\n","symbol":"N$"},{"currency":"Nepalese Rupee","symbol":"रू"},{"currency":"CFP Franc (South Pacific Franc)","symbol":"F"},{"currency":"Nicaraguan Córdoba (córdoba oro)","symbol":"C$"},{"currency":"West African CFA Franc","symbol":"CFA"},{"currency":"Nigerian Naira","symbol":"₦"},{"currency":"Macedonian Denar","symbol":"Ден"},{"currency":"Omani Rial","symbol":"﷼."},{"currency":"Pakistan Rupee","symbol":"Rp"},{"currency":"Panamanian Balboa\r\n","symbol":"B/."},{"currency":"Papua New Guinean Kina","symbol":"K"},{"currency":"Paraguayan Guarani","symbol":"₲"},{"currency":"Peruvian (Nuevo) Sol","symbol":"S/"},{"currency":"Philippine Peso","symbol":"₱"},{"currency":"Polish Zloty (złoty)","symbol":"zł"},{"currency":"Qatari Rial","symbol":"ر.ق"},{"currency":"Leu","symbol":"lei"},{"currency":"Russian Ruble\r\n Russian Ruble","symbol":"₽"},{"currency":"Rwanda Franc","symbol":"RF"},{"currency":"Saint Helena Pound","symbol":"£"},{"currency":"Tala","symbol":"WS$"},{"currency":"Dobra","symbol":"Db"},{"currency":"Saudi Riyal","symbol":"﷼"},{"currency":"Serbian Dinar","symbol":"din"},{"currency":"Seychelles Rupee","symbol":"SRe"},{"currency":"Leone","symbol":"Le"},{"currency":"Singapore Dollar","symbol":"S$"},{"currency":"Antillean guilder","symbol":"ƒ"},{"currency":"Solomon Islands Dollar","symbol":"Si$"},{"currency":"Somali Shilling","symbol":"Sh.so."},{"currency":"Rand","symbol":"R"},{"currency":"","symbol":"£"},{"currency":"South Sudanese pound\r\n since 18. July 2011","symbol":"ج.س."},{"currency":"Sri Lanka Rupee","symbol":"ரூ"},{"currency":"Sudanese Dinar [obsolete]\r\n since 10. Jan. 2007 Sudanese Pound","symbol":"ج.س."},{"currency":"Suriname Guilder","symbol":"$"},{"currency":"Lilangeni","symbol":"kr."},{"currency":"Swedish Krona","symbol":"kr"},{"currency":"Swiss Franc","symbol":"Fr"},{"currency":"Syrian Pound","symbol":"£"},{"currency":"New Taiwan Dollar","symbol":"NT$"},{"currency":"Somoni","symbol":"ЅM"},{"currency":"Tanzanian Shilling","symbol":"TSh"},{"currency":"Thai Baht","symbol":"฿"},{"currency":"Paʻanga","symbol":"T$"},{"currency":"Trinidad and Tobago Dollar","symbol":"TT$"},{"currency":"Tunisian Dinar","symbol":"TD"},{"currency":"Turkish Lira","symbol":"₺"},{"currency":"Manat","symbol":"T"},{"currency":"Tuvaluan Dollar","symbol":"TV$"},{"currency":"Uganda Shilling","symbol":"USh"},{"currency":"Ukrainian Hryvnia","symbol":"₴"},{"currency":"UAE Dirham","symbol":"د.إ"},{"currency":"Pound Sterling","symbol":"£"},{"currency":"Uruguayan Peso","symbol":"$U"},{"currency":"Uzbekistan Sum","symbol":"so'm"},{"currency":"Vanuatu Vatu","symbol":"VT"},{"currency":"Venezuelan Bolívar","symbol":"Bs"},{"currency":"Vietnamese Dong","symbol":"₫"},{"currency":"CFP Franc","symbol":"F"},{"currency":"Moroccan Dirham","symbol":"₧"},{"currency":"Yemeni Rial","symbol":"﷼."},{"currency":"Zambian Kwacha","symbol":"ZK"},{"currency":"Zimbabwe Dollar","symbol":"Z$"}]}
+window.currencyNameToSymbol = {
+  currencyNameToSymbol: [
+    {
+      currency: 'Afghan Afghani',
+      symbol: '؋'
+    },
+    {
+      currency: 'Euro',
+      symbol: '€'
+    },
+    {
+      currency: 'Albanian Lek',
+      symbol: 'L'
+    },
+    {
+      currency: 'Algerian Dinar',
+      symbol: 'دج'
+    },
+    {
+      currency: 'US Dollar',
+      symbol: '$'
+    },
+    {
+      currency: 'Angolan Kwanza',
+      symbol: 'Kz'
+    },
+    {
+      currency: 'East Caribbean Dollar',
+      symbol: 'EC$'
+    },
+    {
+      currency: 'Argentine Peso',
+      symbol: '$'
+    },
+    {
+      currency: 'Armenian Dram',
+      symbol: '֏'
+    },
+    {
+      currency: 'Aruban Florin',
+      symbol: 'ƒ'
+    },
+    {
+      currency: 'Australian Dollar',
+      symbol: 'AU$'
+    },
+    {
+      currency: 'Azerbaijan Manat',
+      symbol: '₼'
+    },
+    {
+      currency: 'Bahamian Dollar',
+      symbol: 'B$'
+    },
+    {
+      currency: 'Bahraini Dinar',
+      symbol: '.د.ب'
+    },
+    {
+      currency: 'Bangladeshi Taka',
+      symbol: 'Tk'
+    },
+    {
+      currency: 'Barbados Dollar',
+      symbol: 'Bds$'
+    },
+    {
+      currency: 'Belarusian Ruble',
+      symbol: 'Br'
+    },
+    {
+      currency: 'Belize Dollar',
+      symbol: 'BZ$'
+    },
+    {
+      currency: 'CFA Franc',
+      symbol: 'CFA'
+    },
+    {
+      currency: 'Bermudian Dollar',
+      symbol: 'BD$'
+    },
+    {
+      currency: 'Bhutanese Ngultrum',
+      symbol: 'Nu.'
+    },
+    {
+      currency: 'Boliviano',
+      symbol: 'Bs'
+    },
+    {
+      currency: 'Convertible Mark',
+      symbol: 'KM'
+    },
+    {
+      currency: 'Pula',
+      symbol: 'P'
+    },
+    {
+      currency: 'Norwegian Krone',
+      symbol: 'kr'
+    },
+    {
+      currency: 'Brazilian Real',
+      symbol: 'R$'
+    },
+    {
+      currency: 'Brunei Dollar',
+      symbol: 'B$'
+    },
+    {
+      currency: 'Bulgarian Lev',
+      symbol: 'Лв.'
+    },
+    {
+      currency: 'CFA Franc BCEAO',
+      symbol: 'CFA'
+    },
+    {
+      currency: 'Burundi Franc',
+      symbol: 'FBu'
+    },
+    {
+      currency: 'Cape Verde Escudo',
+      symbol: '$'
+    },
+    {
+      currency: 'Cambodian Riel',
+      symbol: '៛'
+    },
+    {
+      currency: 'CFA Franc BEAC',
+      symbol: 'FCFA'
+    },
+    {
+      currency: 'Canadian Dollar',
+      symbol: 'C$'
+    },
+    {
+      currency: 'Cayman Islands Dollar',
+      symbol: 'CI$'
+    },
+    {
+      currency: 'Chilean Peso',
+      symbol: '$'
+    },
+    {
+      currency: 'Renminbi (Yuan)',
+      symbol: '¥'
+    },
+    {
+      currency: 'Colombian Peso',
+      symbol: '$'
+    },
+    {
+      currency: 'Comoro Franc',
+      symbol: 'CF'
+    },
+    {
+      currency: 'CFA Franc BEAC',
+      symbol: 'FC'
+    },
+    {
+      currency: 'Franc Congolais',
+      symbol: 'FC'
+    },
+    {
+      currency: 'New Zealand Dollar',
+      symbol: 'NZ$'
+    },
+    {
+      currency: 'Costa Rican Colón',
+      symbol: '₡'
+    },
+    {
+      currency: 'Croatian Kuna',
+      symbol: 'kn'
+    },
+    {
+      currency: 'Cuban Peso',
+      symbol: '$'
+    },
+    {
+      currency: 'Antillean Guilder',
+      symbol: 'NAƒ'
+    },
+    {
+      currency: 'Czech Koruna',
+      symbol: 'Kč'
+    },
+    {
+      currency: 'Danish Kroner',
+      symbol: 'Kr.'
+    },
+    {
+      currency: 'Djibouti Franc',
+      symbol: 'Fdj'
+    },
+    {
+      currency: 'Dominican Peso',
+      symbol: 'RD$'
+    },
+    {
+      currency: 'Egyptian Pound',
+      symbol: 'E£'
+    },
+    {
+      currency: 'Nakfa',
+      symbol: 'Nfk'
+    },
+    {
+      currency: 'Swazi lilangeni',
+      symbol: 'L'
+    },
+    {
+      currency: 'Ethiopian Birr',
+      symbol: 'Br'
+    },
+    {
+      currency: 'Falkland Islands pound',
+      symbol: '£'
+    },
+    {
+      currency: 'Faroese króna (notes only), the Danish krone is also in use',
+      symbol: 'Kr.'
+    },
+    {
+      currency: 'Fiji Dollar',
+      symbol: 'FJ$'
+    },
+    {
+      currency: 'CFP Franc',
+      symbol: '₣'
+    },
+    {
+      currency: 'Dalasi',
+      symbol: 'D'
+    },
+    {
+      currency: 'Lari',
+      symbol: 'ლ'
+    },
+    {
+      currency: 'Cedi',
+      symbol: 'GH₵'
+    },
+    {
+      currency: 'Gibraltar Pound',
+      symbol: '£'
+    },
+    {
+      currency: 'Danish Krone',
+      symbol: 'Kr.'
+    },
+    {
+      currency: 'Quetzal',
+      symbol: 'GTQ'
+    },
+    {
+      currency: 'Guernsey pound',
+      symbol: '£'
+    },
+    {
+      currency: 'Guinea Franc',
+      symbol: 'FG'
+    },
+    {
+      currency: 'Guyana Dollar',
+      symbol: 'GY$'
+    },
+    {
+      currency: 'Haitian Gourde\r\n',
+      symbol: 'G'
+    },
+    {
+      currency: 'Lempira',
+      symbol: 'L'
+    },
+    {
+      currency: 'Hong Kong Dollar',
+      symbol: 'HK$'
+    },
+    {
+      currency: 'Forint',
+      symbol: 'Ft'
+    },
+    {
+      currency: 'Iceland Krona',
+      symbol: 'Íkr'
+    },
+    {
+      currency: 'Indian Rupee',
+      symbol: '₹'
+    },
+    {
+      currency: 'Rupiah',
+      symbol: 'Rp'
+    },
+    {
+      currency: 'Iranian Rial',
+      symbol: '﷼'
+    },
+    {
+      currency: 'Iraqi Dinar',
+      symbol: 'ع.د'
+    },
+    {
+      currency: 'Manx pound',
+      symbol: '£'
+    },
+    {
+      currency: 'New Israeli Sheqel',
+      symbol: '₪'
+    },
+    {
+      currency: 'Jamaican Dollar',
+      symbol: 'J$'
+    },
+    {
+      currency: 'Yen',
+      symbol: '円'
+    },
+    {
+      currency: 'Jersey pound',
+      symbol: '£'
+    },
+    {
+      currency: 'Jordanian Dinar',
+      symbol: 'د.أ'
+    },
+    {
+      currency: 'Tenge',
+      symbol: '₸'
+    },
+    {
+      currency: 'Kenyan Shilling',
+      symbol: 'K'
+    },
+    {
+      currency: 'North Korean Won',
+      symbol: '₩'
+    },
+    {
+      currency: 'Won',
+      symbol: '₩'
+    },
+    {
+      currency: 'Kuwaiti Dinar',
+      symbol: 'د.ك'
+    },
+    {
+      currency: 'Som',
+      symbol: 'Лв'
+    },
+    {
+      currency: 'Kip',
+      symbol: '₭'
+    },
+    {
+      currency: 'Lebanese Pound',
+      symbol: 'ل.ل.'
+    },
+    {
+      currency: 'Loti (plural Maloti). It is equivalent to the South African Rand.',
+      symbol: 'L'
+    },
+    {
+      currency: 'Liberian Dollar',
+      symbol: 'L$'
+    },
+    {
+      currency: 'Lybian Dinar',
+      symbol: 'ل.د'
+    },
+    {
+      currency: 'Swiss Franc',
+      symbol: 'CHF'
+    },
+    {
+      currency: 'Macanese Pataca',
+      symbol: 'MOP$'
+    },
+    {
+      currency: 'Malagasy Franc',
+      symbol: 'Ar'
+    },
+    {
+      currency: 'Kwacha',
+      symbol: 'MK'
+    },
+    {
+      currency: 'Malaysian Ringgit',
+      symbol: 'RM'
+    },
+    {
+      currency: 'Rufiyaa',
+      symbol: '.ރ'
+    },
+    {
+      currency: 'Maltese Lira',
+      symbol: '€'
+    },
+    {
+      currency: 'Ouguiya',
+      symbol: 'UM'
+    },
+    {
+      currency: 'Mauritius Rupee',
+      symbol: '₨'
+    },
+    {
+      currency: 'Mexican Peso',
+      symbol: 'Mex$'
+    },
+    {
+      currency: 'Moldovan Leu',
+      symbol: 'L'
+    },
+    {
+      currency: 'Mongolia Tughrik',
+      symbol: '₮'
+    },
+    {
+      currency: 'Moroccan Dirham',
+      symbol: '. د. م'
+    },
+    {
+      currency: 'Mozambican Metical',
+      symbol: 'MT'
+    },
+    {
+      currency: 'Burmese Kyat',
+      symbol: 'K'
+    },
+    {
+      currency: 'Namibia Dollar\r\n',
+      symbol: 'N$'
+    },
+    {
+      currency: 'Nepalese Rupee',
+      symbol: 'रू'
+    },
+    {
+      currency: 'CFP Franc (South Pacific Franc)',
+      symbol: 'F'
+    },
+    {
+      currency: 'Nicaraguan Córdoba (córdoba oro)',
+      symbol: 'C$'
+    },
+    {
+      currency: 'West African CFA Franc',
+      symbol: 'CFA'
+    },
+    {
+      currency: 'Nigerian Naira',
+      symbol: '₦'
+    },
+    {
+      currency: 'Macedonian Denar',
+      symbol: 'Ден'
+    },
+    {
+      currency: 'Omani Rial',
+      symbol: '﷼.'
+    },
+    {
+      currency: 'Pakistan Rupee',
+      symbol: 'Rp'
+    },
+    {
+      currency: 'Panamanian Balboa\r\n',
+      symbol: 'B/.'
+    },
+    {
+      currency: 'Papua New Guinean Kina',
+      symbol: 'K'
+    },
+    {
+      currency: 'Paraguayan Guarani',
+      symbol: '₲'
+    },
+    {
+      currency: 'Peruvian (Nuevo) Sol',
+      symbol: 'S/'
+    },
+    {
+      currency: 'Philippine Peso',
+      symbol: '₱'
+    },
+    {
+      currency: 'Polish Zloty (złoty)',
+      symbol: 'zł'
+    },
+    {
+      currency: 'Qatari Rial',
+      symbol: 'ر.ق'
+    },
+    {
+      currency: 'Leu',
+      symbol: 'lei'
+    },
+    {
+      currency: 'Russian Ruble\r\n Russian Ruble',
+      symbol: '₽'
+    },
+    {
+      currency: 'Rwanda Franc',
+      symbol: 'RF'
+    },
+    {
+      currency: 'Saint Helena Pound',
+      symbol: '£'
+    },
+    {
+      currency: 'Tala',
+      symbol: 'WS$'
+    },
+    {
+      currency: 'Dobra',
+      symbol: 'Db'
+    },
+    {
+      currency: 'Saudi Riyal',
+      symbol: '﷼'
+    },
+    {
+      currency: 'Serbian Dinar',
+      symbol: 'din'
+    },
+    {
+      currency: 'Seychelles Rupee',
+      symbol: 'SRe'
+    },
+    {
+      currency: 'Leone',
+      symbol: 'Le'
+    },
+    {
+      currency: 'Singapore Dollar',
+      symbol: 'S$'
+    },
+    {
+      currency: 'Antillean guilder',
+      symbol: 'ƒ'
+    },
+    {
+      currency: 'Solomon Islands Dollar',
+      symbol: 'Si$'
+    },
+    {
+      currency: 'Somali Shilling',
+      symbol: 'Sh.so.'
+    },
+    {
+      currency: 'Rand',
+      symbol: 'R'
+    },
+    {
+      currency: '',
+      symbol: '£'
+    },
+    {
+      currency: 'South Sudanese pound\r\n since 18. July 2011',
+      symbol: 'ج.س.'
+    },
+    {
+      currency: 'Sri Lanka Rupee',
+      symbol: 'ரூ'
+    },
+    {
+      currency: 'Sudanese Dinar [obsolete]\r\n since 10. Jan. 2007 Sudanese Pound',
+      symbol: 'ج.س.'
+    },
+    {
+      currency: 'Suriname Guilder',
+      symbol: '$'
+    },
+    {
+      currency: 'Lilangeni',
+      symbol: 'kr.'
+    },
+    {
+      currency: 'Swedish Krona',
+      symbol: 'kr'
+    },
+    {
+      currency: 'Swiss Franc',
+      symbol: 'Fr'
+    },
+    {
+      currency: 'Syrian Pound',
+      symbol: '£'
+    },
+    {
+      currency: 'New Taiwan Dollar',
+      symbol: 'NT$'
+    },
+    {
+      currency: 'Somoni',
+      symbol: 'ЅM'
+    },
+    {
+      currency: 'Tanzanian Shilling',
+      symbol: 'TSh'
+    },
+    {
+      currency: 'Thai Baht',
+      symbol: '฿'
+    },
+    {
+      currency: 'Paʻanga',
+      symbol: 'T$'
+    },
+    {
+      currency: 'Trinidad and Tobago Dollar',
+      symbol: 'TT$'
+    },
+    {
+      currency: 'Tunisian Dinar',
+      symbol: 'TD'
+    },
+    {
+      currency: 'Turkish Lira',
+      symbol: '₺'
+    },
+    {
+      currency: 'Manat',
+      symbol: 'T'
+    },
+    {
+      currency: 'Tuvaluan Dollar',
+      symbol: 'TV$'
+    },
+    {
+      currency: 'Uganda Shilling',
+      symbol: 'USh'
+    },
+    {
+      currency: 'Ukrainian Hryvnia',
+      symbol: '₴'
+    },
+    {
+      currency: 'UAE Dirham',
+      symbol: 'د.إ'
+    },
+    {
+      currency: 'Pound Sterling',
+      symbol: '£'
+    },
+    {
+      currency: 'Uruguayan Peso',
+      symbol: '$U'
+    },
+    {
+      currency: 'Uzbekistan Sum',
+      symbol: 'so\'m'
+    },
+    {
+      currency: 'Vanuatu Vatu',
+      symbol: 'VT'
+    },
+    {
+      currency: 'Venezuelan Bolívar',
+      symbol: 'Bs'
+    },
+    {
+      currency: 'Vietnamese Dong',
+      symbol: '₫'
+    },
+    {
+      currency: 'CFP Franc',
+      symbol: 'F'
+    },
+    {
+      currency: 'Moroccan Dirham',
+      symbol: '₧'
+    },
+    {
+      currency: 'Yemeni Rial',
+      symbol: '﷼.'
+    },
+    {
+      currency: 'Zambian Kwacha',
+      symbol: 'ZK'
+    },
+    {
+      currency: 'Zimbabwe Dollar',
+      symbol: 'Z$'
+    }
+  ]
+}
--- resources/static/js/dashboard/module/settings.js
+++ resources/static/js/dashboard/module/settings.js
@@ -173,8 +173,16 @@
 
 	}
 
+	/*
+	*	Country Drop down Populate
+	*/
+
 	/*An array containing all the country names in the world:*/
-	let countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua & Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia & Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre & Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts & Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad & Tobago","Tunisia","Turkey","Turkmenistan","Turks & Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+	let countries = [];
+	let locToCou = window.localeToCountry.localeToCountry;
+	for(let i = 0, l = locToCou.length; i < l; i++) {
+		countries.push(locToCou[i].name);
+	}
 
 	/*initiate the autocomplete function on the "chooseCountryInp" element, and pass along the countries array as possible autocomplete values:*/
 	autocomplete(document.getElementById("chooseCountryInp"), countries);
@@ -189,6 +197,26 @@
 	$('#chooseCountryDD dropdown-item').click(function () {      
 		document.getElementById('chosenCountry').innerText = this.getElementsByTagName('input').value;
 		// TODO Process Change country request
+	});
+
+	/*
+	*	Currency Dropdown Populate
+	*/
+
+	/*An array containing all the currency names in the world:*/
+	let currencies = [];
+	let curToSym = window.currencyNameToSymbol.currencyNameToSymbol;
+	for(let i = 0, l = curToSym.length; i < l; i++) {
+		currencies.push(curToSym[i].currency);
+	}
+
+	/*initiate the autocomplete function on the "chooseCurrencyInp" element, and pass along the countries array as possible autocomplete values:*/
+	autocomplete(document.getElementById("chooseCurrencyInp"), currencies);
+
+	// On click drop down btn of country search
+	$("#chosenCurrencyDropdown").on("shown.bs.dropdown", function(event){
+		// Input change focus to the country search bar 
+		document.getElementById('chooseCurrencyInp').focus();
 	});
 
 }(jQuery));
