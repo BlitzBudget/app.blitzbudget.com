5a981d57bb513ee6ff3757f67f769363aef1d75b
Arjun <nagarjun_nagesh@outlook.com>
2020-01-12 14:24:56 +0100

    auto complete module scrol down / up effect

--- resources/static/js/dashboard/module/settings.js
+++ resources/static/js/dashboard/module/settings.js
@@ -58,7 +58,7 @@
 	/**
 	* Autocomplete Module
 	**/
-	function autocomplete(inp, arr) {
+	function autocomplete(inp, arr, scrollWrapEl) {
 	  /*the autocomplete function takes two arguments,
 	  the text field element and an array of possible autocompleted values:*/
 	  let currentFocus;
@@ -67,7 +67,6 @@
 	      let a, b, i, val = this.value,  len = arr.length, upperVal, startsWithChar, regVal;
 	      /*close any already open lists of autocompleted values*/
 	      closeAllLists();
-	      debugger;
 	      if (!val) {
 	      	len = arr.length < 5 ? arr.length : 5;
 	      } else {
@@ -83,7 +82,6 @@
 	      /*for each item in the array...*/
 	      for (let i = 0; i < len; i++) {
 	      	let autoFilEl = false;
-	      	
 		  	if(!val) {
 		  		autoFilEl = true;
 		  	} else {
@@ -97,8 +95,9 @@
 		        }	
 		  	}
 
+		  	// Confinue with the iteration
 		  	if(!autoFilEl) {
-		  		return false;
+		  		continue;
 		  	}
 	        
 	        /*create a DIV element for each matching element:*/
@@ -106,23 +105,20 @@
 	        b.classList.add("dropdown-item");
 	        /*make the matching letters bold:*/
 	        if(startsWithChar) {
-	          	b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-	          	b.innerHTML += arr[i].substr(val.length);
+	          	b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>" + arr[i].substr(val.length);
 	        } else if(!val) {
 	        	b.innerHTML = arr[i];
 	        } else {
 	          	let startPos = regVal.exec(arr[i].toUpperCase()).index;
 	          	let startPos2 = startPos + val.length;
-	          	b.innerHTML = arr[i].substr(0, startPos);
-	          	b.innerHTML += "<strong>" + arr[i].substr(startPos, val.length) + "</strong>";
-	          	b.innerHTML += arr[i].substr(startPos2);
+	          	b.innerHTML = arr[i].substr(0, startPos) + "<strong>" + arr[i].substr(startPos, val.length) + "</strong>" + arr[i].substr(startPos2);
 	        }
 	        /*insert a input field that will hold the current array item's value:*/
 	        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
 	        /*execute a function when someone clicks on the item value (DIV element):*/
 	        b.addEventListener("click", function(e) {
 	              /*insert the value for the autocomplete text field:*/
-	              inp.value = this.getElementsByTagName("input")[0].value;
+	              if(isNotEmpty(inp)) inp.value = this.getElementsByTagName("input")[0].value;
 	              /*close the list of autocompleted values,
 	              (or any other open lists of autocompleted values:*/
 	              closeAllLists();
@@ -132,7 +128,8 @@
 	  });
 	  /*execute a function presses a key on the keyboard:*/
 	  inp.addEventListener("keydown", function(e) {
-	      let x = document.getElementById(this.id + "autocomplete-list");
+	  	  let wrapClassId = this.id + "autocomplete-list";
+	      let x = document.getElementById(wrapClassId);
 	      if (x) x = x.getElementsByTagName("div");
 	      if (e.keyCode == 40) {
 	        /*If the arrow DOWN key is pressed,
@@ -154,6 +151,10 @@
 	          if (x) x[currentFocus].click();
 	        }
 	      }
+	      /* set equal to the position of the selected element minus the height of scrolling div */
+	      let scrollToEl = $("#" + scrollWrapEl);
+	      scrollToEl.scrollTop(0);//set to top
+    	  scrollToEl.scrollTop($('#' + wrapClassId + ' .autocomplete-active').offset().top - scrollToEl.offset().top + scrollToEl.scrollTop());
 	  });
 	  function addActive(x) {
 	    /*a function to classify an item as "active":*/
@@ -183,7 +184,6 @@
 	      }
 	    }
 	  }
-	  // TODO trigger key press event
 	}
 
 	/*
@@ -209,12 +209,20 @@
 	}
 
 	/*initiate the autocomplete function on the "chosenCountryInp" element, and pass along the countries array as possible autocomplete values:*/
-	autocomplete(document.getElementById("chosenCountryInp"), countries);
+	autocomplete(document.getElementById("chosenCountryInp"), countries, "chooseCountryDD");
 
 	// On click drop down btn of country search
 	$("#chosenCountryDropdown").on("shown.bs.dropdown", function(event){
+		let countryInp = document.getElementById('chosenCountryInp');
 		// Input change focus to the country search bar 
-		document.getElementById('chosenCountryInp').focus();
+		countryInp.focus();
+		// Trigger input event
+		let eventInp = new Event('input', {
+		    bubbles: true,
+		    cancelable: true,
+		});
+
+		countryInp.dispatchEvent(eventInp);
 	});
 
 	// On click drop down btn of country search
@@ -231,7 +239,7 @@
 	    except the one passed as an argument:*/
 	    let x = elmnt.getElementsByClassName("autocomplete-items");
 	    for (let i = 0, len = x.length; i < len; i++) {
-	      if (elmnt != x[i] && elmnt != inp) {
+	      if (elmnt != x[i]) {
 	        x[i].parentNode.removeChild(x[i]);
 	      }
 	    }
@@ -308,19 +316,27 @@
 		if(isEqual(currentUser.currency,curToSym[i].symbol)) {
 			document.getElementById('chosenCurrency').innerText = curToSym[i].currency;
 			// To be used to display "with wallet" section
-			document.getElementById('currentCurrencies').appendChild(dropdownItemsWithWallet(locToCou[i].name));
+			document.getElementById('currentCurrencies').appendChild(dropdownItemsWithWallet(curToSym[i].currency));
 		} else {
 			currencies.push(curToSym[i].currency);
 		}
 	}
 
 	/*initiate the autocomplete function on the "chosenCurrencyInp" element, and pass along the countries array as possible autocomplete values:*/
-	autocomplete(document.getElementById("chosenCurrencyInp"), currencies);
+	autocomplete(document.getElementById("chosenCurrencyInp"), currencies, "chooseCurrencyDD");
 
 	// On click drop down btn of country search
 	$("#chosenCurrencyDropdown").on("shown.bs.dropdown", function(event){
+		let currencyInp = document.getElementById('chosenCurrencyInp');
 		// Input change focus to the country search bar 
-		document.getElementById('chosenCurrencyInp').focus();
+		currencyInp.focus();
+		// Trigger input event
+		let eventInp = new Event('input', {
+		    bubbles: true,
+		    cancelable: true,
+		});
+
+		currencyInp.dispatchEvent(eventInp);
 	});
 
 	// On click drop down btn of country search
