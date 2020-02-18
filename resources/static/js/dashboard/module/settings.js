6ea5560dfce6c3a4e237e3fd3282e5d842237946
C0625509 <nagarjun_nagesh@carrefour.com>
2020-02-18 14:44:02 +0100

    convert from json data to xls

--- resources/static/js/dashboard/module/settings.js
+++ resources/static/js/dashboard/module/settings.js
@@ -1,5 +1,4 @@
 "use strict";
-
 (function scopeWrapper($) {
 	// Custom Javascript for dashboard
 	const SETTINGS_CONSTANTS = {};
@@ -283,7 +282,6 @@
 
 	// Update user attributes
 	function updateUserAttr(param, paramVal, event, valObj) {
-		debugger;
 		let oldValInTe = '';
 		let inpId = '';
 		// Current countries and current currencies then do
