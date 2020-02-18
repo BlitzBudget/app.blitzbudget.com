76b2c61d15b3af380f86dbf6e61a33a53ddaa11a
C0625509 <nagarjun_nagesh@carrefour.com>
2020-02-18 17:37:53 +0100

    export as doc func

--- resources/static/js/dashboard/income/transactions/export/exportSelectedToDOC.js
+++ resources/static/js/dashboard/income/transactions/export/exportSelectedToDOC.js
@@ -1,24 +1,27 @@
 "use strict";
 (function scopeWrapper($) {
+   let header = "<html xmlns:o='urn:schemas-microsoft-com:office:office' "+
+            "xmlns:w='urn:schemas-microsoft-com:office:word' "+
+            "xmlns='http://www.w3.org/TR/REC-html40'>"+
+            "<head><meta charset='utf-8'><title>Export HTML to Word Document with JavaScript</title></head><body>";
+
+   let footer = "</body></html>";
+
     document.getElementById('downloadTransactionsData').addEventListener("click",function(e){
       // Export to word
       exportToWord();
     });
 
     function exportToWord() {
-      var link, blob, url;
-      blob = new Blob(['\ufeff', document.getElementById("productsJson").innerHTML], {
-            type: 'application/msword'
-      });
-      url = URL.createObjectURL(blob);
-      link = document.createElement('a');
-      link.classList = 'invisible';
-      link.href = url;
-      link.download = 'transactions';  // default name without extension 
-      document.body.appendChild(link);
-      if (navigator.msSaveOrOpenBlob )
-           navigator.msSaveOrOpenBlob( blob, 'transactions.docx'); // IE10-11
-      else link.click();  // other browsers
-      document.body.removeChild(link);
-    };
+      let sourceHTML = header+document.getElementById("productsJson").innerHTML+footer;
+
+      let source = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(sourceHTML);
+      let fileDownload = document.createElement("a");
+      document.body.appendChild(fileDownload);
+      fileDownload.classList = 'invisible';
+      fileDownload.href = source;
+      fileDownload.download = 'document.doc';
+      fileDownload.click();
+      document.body.removeChild(fileDownload);
+    }
 }(jQuery));
