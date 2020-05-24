f847ec3aa11cc8cd906d52c0faa0a9fc8749a20a
C0625509 <nagarjun_nagesh@carrefour.com>
2020-05-24 12:42:37 +0200

    drag and drop

--- resources/static/js/dashboard/income/transactions/dragAndDropTransInAcc.js
+++ resources/static/js/dashboard/income/transactions/dragAndDropTransInAcc.js
@@ -233,16 +233,12 @@
 			// Fetch the current account balance
 			let oldAccDiv = document.getElementById('accountBalance-' + oldAccountId);
 			let oldAccBal = er.convertToNumberFromCurrency(oldAccDiv.innerText,currentCurrencyPreference);
-			// If the acc balance is negative then
-			if(~oldAccDiv.innerText.indexOf("-")) oldAccBal *= -1;
 			let accDiv = document.getElementById('accountBalance-' + accountId);
 			let accBal = er.convertToNumberFromCurrency(accDiv.innerText,currentCurrencyPreference);
-			// If the acc balance is negative then
-			if(~accDiv.innerText.indexOf("-")) accBal *= -1;
 			let currAccBal = 0;
 			let currNewAccBal = 0;
         	// Append a - sign if it is an expense
-			currAccBal = oldAccBal - userTransaction.amount
+			currAccBal = oldAccBal + userTransaction.amount
 			currNewAccBal = accBal + userTransaction.amount;
 			// Append the new amount to the front
 			oldAccDiv.innerText = formatToCurrency(currAccBal);
