51680ef85446b43d5c5d025deb1813867dde89c4
Minnu Ashok Kumar <minnuashokkumar@Minnus-MacBook-Air.local>
2020-06-27 01:05:50 +0200

    Goals

--- resources/static/js/dashboard/module/goals.js
+++ resources/static/js/dashboard/module/goals.js
@@ -2,32 +2,65 @@
 (function scopeWrapper($) {
 
     /**
-	*  Add Functionality Generic + Btn
-	**/
+     * START loading the page
+     *
+     */
+    let currentPageInCookie = er.getCookie('currentPage');
+    if (isEqual(currentPageInCookie, 'goalsPage')) {
+        if (isEqual(window.location.href, window._config.app.invokeUrl)) {
+            populateCurrentPage('goalsPage');
+        }
+    }
 
-	// Register Tooltips
-	let ttinit = $("#addFncTT");
-	ttinit.attr('data-original-title', 'Add Goals');
-	ttinit.tooltip({
-		delay: { "show": 300, "hide": 100 }
-    });
+    /*
+     * On Click goals
+     */
+    let goalsPage = document.getElementById('goalsPage');
+    if (isNotEmpty(settingsPage)) {
+        goalsPage.addEventListener("click", function (e) {
+            populateCurrentPage('goalsPage');
+        });
+    }
 
-    // Generic Add Functionality
-    let genericAddFnc = document.getElementById('genericAddFnc');
-    document.getElementById('addFncTT').textContent = 'add';
-    genericAddFnc.classList = 'btn btn-round btn-warning btn-just-icon bottomFixed float-right addNewGoals';
-    $(genericAddFnc).unbind('click').click(function () {
-    	if(!this.classList.contains('addNewGoals')) {
-    		return;
-    	}
+    function populateCurrentPage(page) {
+        er.refreshCookiePageExpiry(page);
+        er.fetchCurrentPage('/goals', function (data) {
+            // Load the new HTML
+            $('#mutableDashboard').html(data);
+            // Translate current Page
+            translatePage(getLanguage());
+            // Set Current Page
+            let currentPage = document.getElementById('currentPage');
+            currentPage.setAttribute('data-i18n', 'goals.page.title');
+            currentPage.textContent = 'Goals';
+        });
 
-    	// Create goals
-		$('#goalsModal').modal('toggle');
-    });
+        /**
+         *  Add Functionality Generic + Btn
+         **/
 
-    // Fetch all bank account information
-    er_a.fetchAllBankAccountInfo(function(bankAccountList) {
+        // Register Tooltips
+        let ttinit = $("#addFncTT");
+        ttinit.attr('data-original-title', 'Add Goals');
+        ttinit.tooltip({
+            delay: {
+                "show": 300,
+                "hide": 100
+            }
+        });
 
-    });
-    
-}(jQuery)); 
+        // Generic Add Functionality
+        let genericAddFnc = document.getElementById('genericAddFnc');
+        document.getElementById('addFncTT').textContent = 'add';
+        genericAddFnc.classList = 'btn btn-round btn-warning btn-just-icon bottomFixed float-right addNewGoals';
+        $(genericAddFnc).unbind('click').click(function () {
+            if (!this.classList.contains('addNewGoals')) {
+                return;
+            }
+
+            // Create goals
+            $('#addGoals').modal('toggle');
+        });
+    }
+
+}(jQuery));
