6a7e630148de16ee290f1cadd94f50f2cd2f1386
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2020-06-28 22:36:03 +0200

    All goals initial mock

--- resources/static/js/dashboard/module/goals.js
+++ resources/static/js/dashboard/module/goals.js
@@ -74,14 +74,125 @@
             case "save-for-emergency":
                 saveForEmergency();
                 break;
+            case "pay-off-credit-card":
+                payOffCreditCard();
+                break;
+            case "pay-off-loans":
+                payOffLoans();
+                break;
+            case "save-for-retirement":
+                saveForRetirement()
+                break;
+            case "buy-a-home":
+                buyAHome();
+                break;
+            case "buy-an-automobile":
+                buyAnAutomobile();
+                break;
+            case "save-for-college":
+                saveForCollege();
+                break;
+            case "take-a-trip":
+                takeATrip();
+                break;
+            case "improve-my-home":
+                improveMyHome();
+                break;
+            case "create-a-custom-goal":
+                createACustomGoal();
+                break;
             default:
                 break;
         }
     });
 
+    /*
+     * Save for Emergency
+     */
     function saveForEmergency() {
         document.getElementById('choose-a-goal').classList.add('d-none');
         document.getElementById('save-for-emergency').classList.remove('d-none');
+        document.getElementById('choose-goal-footer').classList.remove('d-none');
+    }
+
+    /*
+     * Pay off credit cards
+     */
+    function payOffCreditCard() {
+        document.getElementById('choose-a-goal').classList.add('d-none');
+        document.getElementById('pay-off-credit-card').classList.remove('d-none');
+        document.getElementById('choose-goal-footer').classList.remove('d-none');
+    }
+
+    /*
+     * Pay off loans
+     */
+    function payOffLoans() {
+        document.getElementById('choose-a-goal').classList.add('d-none');
+        document.getElementById('pay-off-loans').classList.remove('d-none');
+        document.getElementById('choose-goal-footer').classList.remove('d-none');
+    }
+
+    /*
+     * Save for Retirement
+     */
+    function saveForRetirement() {
+        document.getElementById('choose-a-goal').classList.add('d-none');
+        document.getElementById('save-for-retirement').classList.remove('d-none');
+        document.getElementById('choose-goal-footer').classList.remove('d-none');
+    }
+
+    /*
+     * Buy a home
+     */
+    function buyAHome() {
+        document.getElementById('choose-a-goal').classList.add('d-none');
+        document.getElementById('buy-a-home').classList.remove('d-none');
+        document.getElementById('choose-goal-footer').classList.remove('d-none');
+    }
+
+    /*
+     * Buy an automobile
+     */
+    function buyAnAutomobile() {
+        document.getElementById('choose-a-goal').classList.add('d-none');
+        document.getElementById('buy-an-automobile').classList.remove('d-none');
+        document.getElementById('choose-goal-footer').classList.remove('d-none');
+    }
+
+    /*
+     * Take a trip
+     */
+    function takeATrip() {
+        document.getElementById('choose-a-goal').classList.add('d-none');
+        document.getElementById('take-a-trip').classList.remove('d-none');
+        document.getElementById('choose-goal-footer').classList.remove('d-none');
+    }
+
+    /*
+     * Improve my home
+     */
+    function improveMyHome() {
+        document.getElementById('choose-a-goal').classList.add('d-none');
+        document.getElementById('improve-my-home').classList.remove('d-none');
+        document.getElementById('choose-goal-footer').classList.remove('d-none');
+    }
+
+    /*
+     * Create a custom goal
+     */
+    function createACustomGoal() {
+        document.getElementById('choose-a-goal').classList.add('d-none');
+        document.getElementById('create-a-custom-goal').classList.remove('d-none');
+        document.getElementById('choose-goal-footer').classList.remove('d-none');
+    }
+
+    /*
+     * Save for College
+     */
+    function saveForCollege() {
+        document.getElementById('choose-a-goal').classList.add('d-none');
+        document.getElementById('save-for-college').classList.remove('d-none');
         document.getElementById('choose-goal-footer').classList.remove('d-none');
     }
 
@@ -127,6 +238,15 @@
     $('body').on("click", "#back-goals", function (event) {
         document.getElementById('choose-a-goal').classList.remove('d-none');
         document.getElementById('save-for-emergency').classList.add('d-none');
+        document.getElementById('pay-off-credit-card').classList.add('d-none');
+        document.getElementById('pay-off-loans').classList.add('d-none');
+        document.getElementById('save-for-retirement').classList.add('d-none');
+        document.getElementById('buy-a-home').classList.add('d-none');
+        document.getElementById('buy-an-automobile').classList.add('d-none');
+        document.getElementById('save-for-college').classList.add('d-none');
+        document.getElementById('take-a-trip').classList.add('d-none');
+        document.getElementById('improve-my-home').classList.add('d-none');
+        document.getElementById('create-a-custom-goal').classList.add('d-none');
         document.getElementById('choose-goal-footer').classList.add('d-none');
     });
 
