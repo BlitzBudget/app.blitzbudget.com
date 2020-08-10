87a38667426b7e81492b5a7fad8f451ad2c04697
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2020-08-10 16:49:50 +0200

    transactions modal

--- resources/static/js/dashboard/income/transactions/modal/transactionsModal.js
+++ resources/static/js/dashboard/income/transactions/modal/transactionsModal.js
@@ -6,13 +6,13 @@
         // Set transaction Title
         document.getElementById('transactionLabelInModal').textContent = window.transactionsCache[transactionsId].description;
         // transaction Balance Update
-        document.getElementById('transactionAmountEntry').textContent = formatToCurrency(window.transactionsCache[transactionsId].amount);
+        document.getElementById('transactionAmountEntry').value = formatToCurrency(window.transactionsCache[transactionsId].amount);
         // Transaction Category Update
         document.getElementById('transactionCategoryEntry').textContent = window.categoryMap[window.transactionsCache[transactionsId].category].name;
         // Transaction Description Update
-        document.getElementById('transactionDescriptionEntry').textContent = window.transactionsCache[transactionsId].description;
+        document.getElementById('transactionDescriptionEntry').value = window.transactionsCache[transactionsId].description;
         // Transaction Tags Update
-        document.getElementById('transactionTagsEntry').textContent = isNotEmpty(window.transactionsCache[transactionsId].tags) ? window.transactionsCache[transactionsId].tags[0] : "";
+        document.getElementById('transactionTagsEntry').value = isNotEmpty(window.transactionsCache[transactionsId].tags) ? window.transactionsCache[transactionsId].tags[0] : "";
         // Close Account Modal
         document.getElementById('accountInformationMdl').classList.add('d-none');
         // Close  Financial Position
