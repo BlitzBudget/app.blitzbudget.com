c1f23760608aad18f7537a11f1b5cfe4679fefc2
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2020-08-10 11:38:08 +0200

    Transactions Modal

--- resources/static/js/dashboard/income/transactions/modal/transactionsModal.js
+++ resources/static/js/dashboard/income/transactions/modal/transactionsModal.js
@@ -3,10 +3,16 @@
 
     $('body').on('click', '#transactionsTable .recentTransactionEntry', function (e) {
         let transactionsId = this.dataset.target;
-        // Set Category Title
+        // Set transaction Title
         document.getElementById('transactionLabelInModal').textContent = window.transactionsCache[transactionsId].description;
-        // Category Balance Update
-        document.getElementById('transactionAmountEntry').textContent = window.transactionsCache[transactionsId].amount;
+        // transaction Balance Update
+        document.getElementById('transactionAmountEntry').textContent = formatToCurrency(window.transactionsCache[transactionsId].amount);
+        // Transaction Category Update
+        document.getElementById('transactionCategoryEntry').textContent = window.transactionsCache[transactionsId].category;
+        // Transaction Description Update
+        document.getElementById('transactionDescriptionEntry').textContent = window.transactionsCache[transactionsId].description;
+        // Transaction Tags Update
+        document.getElementById('transactionTagsEntry').textContent = isNotEmpty(window.transactionsCache[transactionsId].tags) ? window.transactionsCache[transactionsId].tags[0] : "";
         // Close Account Modal
         document.getElementById('accountInformationMdl').classList.add('d-none');
         // Close  Financial Position
