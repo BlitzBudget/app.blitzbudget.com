783a4a2279d92ab892cf7523a51e2d81c1bee1cb
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2020-08-25 18:45:49 +0200

    add new category modal in add transactions

--- resources/static/js/dashboard/income/transactions/modal/addTransactionsModal.js
+++ resources/static/js/dashboard/income/transactions/modal/addTransactionsModal.js
@@ -2,12 +2,6 @@
 (function scopeWrapper($) {
 
     // Add New Categories
-    $('body').on('click', '#addNewCategory', function (e) {
-        document.getElementById('newCategorySelection').classList.toggle('d-none');
-        document.getElementById('categoryOptions').classList.toggle('d-none');
-    });
-
-    // Add New Categories
     $('body').on('click', '#typeSelection .dropdown-item', function (e) {
         let dropdownValue = this.lastElementChild.value;
         let typeSelection = document.getElementById('typeSelection');
