19c241d2cf51d91b31d425df1a50d5ed9124cfd9
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2020-08-25 15:54:02 +0200

    transactions and on click drop down

--- resources/static/js/dashboard/income/transactions/modal/addTransactionsModal.js
+++ resources/static/js/dashboard/income/transactions/modal/addTransactionsModal.js
@@ -7,4 +7,12 @@
         document.getElementById('categoryOptions').classList.toggle('d-none');
     });
 
+    // Add New Categories
+    $('body').on('click', '#typeSelection .dropdown-item', function (e) {
+        let dropdownValue = this.lastElementChild.value;
+        let typeSelection = document.getElementById('typeSelection');
+        typeSelection.firstElementChild.textContent = window.categoryMap[dropdownValue].name;
+        typeSelection.setAttribute('data-chosen', dropdownValue);
+    });
+
 }(jQuery));
