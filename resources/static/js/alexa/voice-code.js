909a044fedf89df3d01532296195d4fd8bdb29d6
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2020-10-14 21:50:22 +0200

    Voice code show error message

--- resources/static/js/alexa/voice-code.js
+++ resources/static/js/alexa/voice-code.js
@@ -2,7 +2,14 @@
 (function scopeWrapper($) {
 
     document.getElementById("voice-code").addEventListener('keyup', function () {
-
+        let val = this.value;
+        if (isNaN(val)) {
+            this.classList.add('errorborder');
+            document.getElementById('errorFourDigit').classList.remove('d-none');
+        } else {
+            this.classList.remove('errorborder');
+            document.getElementById('errorFourDigit').classList.add('d-none');
+        }
     });
 
 }(jQuery));
