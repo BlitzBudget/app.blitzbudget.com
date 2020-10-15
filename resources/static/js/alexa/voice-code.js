04edf4df8c2d071424412a7d8d70c76aae5eb1cf
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2020-10-15 21:47:22 +0200

    voice code next button

--- resources/static/js/alexa/voice-code.js
+++ resources/static/js/alexa/voice-code.js
@@ -6,10 +6,19 @@
         if (isNaN(val)) {
             this.classList.add('errorborder');
             document.getElementById('errorFourDigit').classList.remove('d-none');
+            document.getElementById('nextButton').setAttribute('disabled', 'disabled');
         } else {
             this.classList.remove('errorborder');
             document.getElementById('errorFourDigit').classList.add('d-none');
+            document.getElementById('nextButton').removeAttribute('disabled');
         }
+    });
+
+    document.getElementById('nextButton').addEventListener('click', function () {
+        let voiceCode = document.getElementById('voice-code-box');
+        voiceCode.classList.add('d-none');
+        let emailBox = document.getElementById('email-box');
+        emailBox.classList.remove('d-none');
     });
 
 }(jQuery));
